6c94ab67708ae36d0381a8b37ded0014acb2e693
Fahad Qazi <fahadqazi@gmail.com>
2016-04-25 00:57:25 +0100

    ng-classifieds project, AngularJS

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,13 @@
+(function(){
+  "use strict"
+  
+  angular
+    .module('ngClassifieds')
+      .controller('classifiedsCtrl', ['$scope',function($scope){
+      
+        $scope.name = "fahad";
+        $scope.message = "hello, world";
+    
+  }]);
+  
+})();
